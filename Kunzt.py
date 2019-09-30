b481e1703f5fcbacbacffe9ce708a3908687d8a8
Max K <Max-MKA@web.de>
2019-09-30 21:49:09 +0200

    New Version, no more Tesseract

--- Kunzt.py
+++ Kunzt.py
@@ -1,79 +1,64 @@
-from PIL import Image, ExifTags, ImageFilter
-import pytesseract
 import sys
 import os
+import io
+import PyPDF2
 
-#filepath = "IMG_3135.jpg"
+#filepath = "Speiseplan Kunzmann.pdf"
 filepath = sys.argv[1]
-image = Image.open(filepath)
 
-pytesseract.pytesseract.tesseract_cmd = os.getenv('LOCALAPPDATA') + r"\Tesseract-OCR\tesseract.exe"
+text_file = io.open("Speiseplan.txt", "w", encoding='utf-8')
 
-try:
-    for orientation in ExifTags.TAGS.keys():
-        if ExifTags.TAGS[orientation] == 'Orientation':
-            break
-    exif = dict(image._getexif().items())
+pdf_file = open(filepath, 'rb')
+read_pdf = PyPDF2.PdfFileReader(pdf_file)
+page = read_pdf.getPage(0)
+text = page.extractText()
 
-    if exif[orientation] == 3:
-        image = image.rotate(180, expand=True)
-    elif exif[orientation] == 6:
-        image = image.rotate(270, expand=True)
-    elif exif[orientation] == 8:
-        image = image.rotate(90, expand=True)
-except (AttributeError, KeyError, IndexError):
-    # cases: image doesn't have exif
-    pass
+text = text.replace('•', '€')
 
-basewidth = 1200
-wpercent = (basewidth / float(image.size[0]))
-hsize = int((float(image.size[1]) * float(wpercent)))
-image = image.resize((basewidth, hsize), Image.ANTIALIAS)
-
-image = image.convert("L")
-threshold = 150
-imageBW = image.point(lambda p: p > threshold and 255)
-#imageBW.save("imageBW.bmp")
-text = pytesseract.image_to_string(image, lang='deu')
-#print(text) # debug
-
-text = text.splitlines()
-
-
-for i in range(20):  # search only top lines
-    weekPos = text[i].find('Woc')
-    if weekPos != -1:
-        weekString = text[i][:weekPos]
-        weekString = weekString.translate({ord(i): None for i in ' ,.'})  # remove unnecessary characters
-        week = [int(s) for s in weekString.split() if s.isdigit()][0]
-        print(str(week) + ". Woche")
-        date = text[i + 1]
-        print(date)
-        print()
+weekPos = text.find('Woc')
+print(text[weekPos-3:weekPos+5])
+print(text[weekPos-3:weekPos+5], file=text_file)
+for i in range(20):
+  if text[weekPos+3+i].isdigit():
+    for n in range(20):
+      if text[weekPos + 28 + n] == ' ':
+        print(text[weekPos+3+i:weekPos+28+n])
+        print(text[weekPos+3+i:weekPos+28+n], file=text_file)
+        print('', file=text_file)
         break
+    break
 
-weekDays = ['onta', 'iensta', 'ittwoc', 'onnersta', 'reita']
+weekDays = ['Monta', 'Diensta', 'Mittwoc', 'Donnersta', 'Freita', 'Zusatz']
 
-for days in weekDays:
-    for i in range(len(text)):
-        dayPos = text[i].find(days)
-        if dayPos != -1:
-            dayString = text[i][dayPos - 1:]
-            print(dayString)
-            # search the next 10 lines for dishes
-            for n in range(i + 1, min(i + 11, len(text))):
-                if any(x in text[n] for x in weekDays):  # stop when next day is reached
-                    break
-                if text[n].find('usatz') != -1: # stop when end of menu is reached
-                    break
-                elif len(text[n]) > 17: #don't show empty lines
-                    dishString = text[n]
-                    #clean up beginning of string
-                    for offset in range(5):
-                        if dishString[offset].isalpha():
-                            break
-                    dishString = dishString[offset:]
-                    print("  ", dishString)
-            break
+for d in range(0, 5):
+  dayPos = text.find(weekDays[d])
+  dayEnd = dayPos+16+text[dayPos+16:].find(' ')
+  print(text[dayPos:dayEnd])
+  print(text[dayPos:dayEnd], file=text_file)
+  for i in range(dayEnd, dayEnd+20):
+    if text[i].isalpha():
+      mealPos = i
+      mealEnd = text[mealPos:mealPos+10].find("Feiert")
+      if mealEnd > -1:
+        print(text[mealPos:mealPos+mealEnd + 8])
+        print(text[mealPos:mealPos+mealEnd + 8], file=text_file)
+      else: # no holiday
+        nextDay = mealPos+text[mealPos:].find(weekDays[d+1])
+        mealEnd = [n for n, x in enumerate(text[mealPos:nextDay]) if x == '€']
+        if mealPos+mealEnd[1] > nextDay:
+          print(text[mealPos:mealPos+mealEnd[1]+1])
+          print(text[mealPos:mealPos+mealEnd[1]+1], file=text_file)
+        else:
+          print(text[mealPos:nextDay])
+          print(text[mealPos:nextDay], file=text_file)
+        #if len(mealEnd) > 2:
+        #  print(text[mealPos + mealEnd[1] + 1:mealPos + mealEnd[3] + 1])
+      break
 
-print()
+
+#text = text[weekPos-3:]
+
+#print(text)
+
+
+text_file.close()
